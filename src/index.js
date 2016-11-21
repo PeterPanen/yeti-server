abd167c71209d12dd654d7379d5a153b7474a65c
Rasmus Knudsen <rakn@tv2.dk>
2016-11-22 00:05:12 +0100

    Add missing punctuation to comments

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-// Root route handler
+// Root route handler.
 app.get('/', (req, res) => res.json({ message: 'SCV good to go, sir!' }));
 
-// Application http listener
+// Application http listener.
 app.listen(PORT);
