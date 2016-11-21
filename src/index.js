1d819867f60f74b53489a26f79b4c1f54db9d518
Rasmus Knudsen <rakn@tv2.dk>
2016-11-22 00:00:10 +0100

    Uppercase port const

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 
 // Instantiate application.
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 // Root route handler
 app.get('/', (req, res) => res.json({ message: 'SCV good to go, sir!' }));
 
 // Application http listener
-app.listen(port);
+app.listen(PORT);
