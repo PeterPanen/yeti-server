ccaee3fe05b9c61e738f93c5c37e753925e3f843
Rasmus Knudsen <rakn@tv2.dk>
2016-12-28 16:59:01 +0100

    Implement nunjucks template engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import path from 'path';
 import express from 'express';
+import nunjucks from 'nunjucks';
 import routes from './routes';
 
 // Instantiate application.
 const app = express();
 const PORT = 3000;
+
+// Configure template engine.
+app.set('view engine', 'html');
+nunjucks.configure(path.join(__dirname, 'views'), {
+  autoescape: true,
+  express: app,
+});
 
 // Mount routes.
 app.use(routes);
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import userRoutes from './routes/user';
 
 const router = express.Router();
 
+router.get('/', (req, res) => res.render('home/index', { title: 'World!' }));
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 
